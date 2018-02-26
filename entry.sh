185501b3bca3a2993f2fa0cc803f6a7dbfb081c1
John Strunk <jstrunk@redhat.com>
2018-02-26 16:13:50 -0500

    Turn on shell echo
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- entry.sh
+++ entry.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-set -e
+set -ex
 
 #-- Run benchmarks from command line or this set as default
 BENCH_TO_RUN=(${@-clone fio maven})
