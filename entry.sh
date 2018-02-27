ff3a38e0204362a9ef82a9ab2400aba1fac2ac64
John Strunk <jstrunk@redhat.com>
2018-02-27 14:28:03 -0500

    Reorder fio runs to prevent error during layout
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- entry.sh
+++ entry.sh
@@ -52,12 +52,12 @@ function bench_fio {
     local FILE
     FILE="${TARGET_PATH}/testfile"
     fio --filesize="${TARGET_CAPACITY_MB}M" --runtime=120s --ioengine=libaio --direct=1 --time_based --stonewall --filename="$FILE" --eta=never \
-        --name=rr4k@qd1 --description="e2e latency via 4k random reads @ qd=1" --iodepth=1 --bs=4k --rw=randread \
+        --name=sw1m@qd32 --description="Bandwidth via 1MB sequential writes @ qd=32" --iodepth=32 --bs=1m --rw=write \
+        --name=sr1m@qd32 --description="Bandwidth via 1MB sequential reads @ qd=32" --iodepth=32 --bs=1m --rw=read
         --name=rw4k@qd1 --description="e2e latency via 4k random writes @ qd=1" --iodepth=1 --bs=4k --rw=randwrite \
-        --name=rr4k@qd32 --description="IOPS via 4k random reads @ qd=32" --iodepth=32 --bs=4k --rw=randread \
+        --name=rr4k@qd1 --description="e2e latency via 4k random reads @ qd=1" --iodepth=1 --bs=4k --rw=randread \
         --name=rw4k@qd32 --description="IOPS via 4k random writes @ qd=32" --iodepth=32 --bs=4k --rw=randwrite \
-        --name=sr1m@qd32 --description="Bandwidth via 1MB sequential reads @ qd=32" --iodepth=32 --bs=1m --rw=read \
-        --name=sw1m@qd32 --description="Bandwidth via 1MB sequential writes @ qd=32" --iodepth=32 --bs=1m --rw=write
+        --name=rr4k@qd32 --description="IOPS via 4k random reads @ qd=32" --iodepth=32 --bs=4k --rw=randread \
     rm -f "$FILE"
 }
 
